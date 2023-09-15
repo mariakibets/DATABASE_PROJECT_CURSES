42f8357ef9ae5a3e3a51ed42975c90240a367986
Mari Kibets <mariakibec7@Gmail.com>
2023-09-15 19:02:49 +0200

    REFERENCES

--- students-db.session.sql
+++ students-db.session.sql
@@ -1,29 +1,14 @@
-CREATE TABLE books (
-    id serial PRIMARY KEY,
-    author varchar (70) CHECK (author != ''),
-    book_name varchar(70) CHECK (book_name != '' ) UNIQUE,
-    year_relesed varchar(4),
-    paint varchar (70),
-    discription text,
-    quantity int CHECK (quantity >= 0),
-    PRIMARY KEY (author, book_name )
+CREATE TABLE orders_to_products (
+    orsder_id int REFERENCES orders(id),
+    product_id int REFERENCES products (id),
+    quantity int,
+    PRIMARY KEY (orsder_id, product_id)
 );
 
-CREATE TABLE products (
-    id serial PRIMARY KEY,
-    brand varchar (200) NOT NULL,
-    model varchar (300) NOT NULL,
-    description text,
-    price numeric (10, 2) NOT NULL CHECK (price > 0),
-    dicount_price numeric (10, 2) CHECK (dicount_price < price)
-);
 
-INSERT INTO products (brand, model, price ) VALUES
-('Tovar 1', 'Model 1', 200),
-('Tovar 2', 'Model 2', 400),
-('Tovar 3', 'Model 3', 600),
-('Tovar 4', 'Model 4', 800);
+INSERT INTO  orders (customer_id) VALUES (1);
 
-ALTER TABLE products ADD CONSTRAINT "unique_brand_model_pair" UNIQUE (brand , model);
-
-ALTER TABLE products ADD COLUMN quantity int;
+INSERT INTO orders_to_products (orsder_id, product_id, quantity) VALUES 
+(1, 4, 2),
+(1, 2, 1),
+(1, 1, 1);
